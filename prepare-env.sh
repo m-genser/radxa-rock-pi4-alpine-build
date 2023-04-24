b76ba0a430b583aa4b115661a1e9ea06e80d8762
Marvin Genser <marvin@genser.me>
2023-04-24 18:34:34 +0200

    Update prepare-env.sh

--- prepare-env.sh
+++ prepare-env.sh
@@ -8,3 +8,15 @@ sudo apt-get install lzop
 
 # Install LSB modules
 sudo apt-get install lsb-core
+
+# Install Lempel-Ziv-Markow algorithm
+sudo apt-get install lzma
+
+# Install multiple precision complex floating-point library development package
+sudo apt-get install libmpc-dev
+
+# U-Boot tools package
+sudo apt-get install u-boot-tools
+
+# Install ncurses library routines
+sudo apt-get install libncurses5-dev:amd64
